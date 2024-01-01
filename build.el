2c64929d84dd859a2207ff30e6611218f183f0ab
Stephen Emblem <stephenaemblem@gmail.com>
2024-01-01 13:06:47 -0500

    Try and fix css

--- build.el
+++ build.el
@@ -6,7 +6,7 @@
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
       org-html-head
-      " <link rel=\"stylesheet\" href=\"./css/simple.css\" />")
+      " <link rel=\"stylesheet\" href=\"/css/simple.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
